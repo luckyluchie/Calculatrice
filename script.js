868a676338527d214fb17d061f8ac201cd57bf10
Luckyluchie <lucie.legat@hotmail.fr>
2020-12-28 16:04:59 +0100

    add fonts file

--- script.js
+++ script.js
@@ -1,31 +1,95 @@
 const calculator = document.querySelector('.calculator')
 const keys = calculator.querySelector('.calculator-keys')
+const display = document.querySelector('.calculator-display')
 
+// fonction qui calcule
+function calculate(n1, operator, n2) {
+  let result = ''
+  
+  if (operator === 'add') {
+    result = parseFloat(n1) + parseFloat(n2)
+  } else if (operator === 'subtract') {
+    result = parseFloat(n1) - parseFloat(n2)
+  } else if (operator === 'multiply') {
+    result = parseFloat(n1) * parseFloat(n2)
+  } else if (operator === 'divide') {
+    result = parseFloat(n1) / parseFloat(n2)
+  }
+  return result
+}
 
 keys.addEventListener('click', function(e){
-  if (e.target.matches === 'button'){
-    // do something
-  }  
+  if (e.target.matches('button')){
+    const key = e.target
+    const action = key.dataset.action
+    const keyContent = key.textContent
+    const displayedNum = display.textContent
+    const previousKeyType = calculator.dataset.previousKeyType
+    Array.from(key.parentNode.children)
+    .forEach(k => k.classList.remove('is-depressed'))
+  
+    
+    // if nb selected
+    if (!action) {
+      if (displayedNum === '0' || previousKeyType === 'operator' || previousKeyType === 'calculate') {
+        display.textContent = keyContent
+      } else {
+        display.textContent = displayedNum + keyContent
+      }
+      calculator.dataset.previousKey = 'number'
+    }
 
-  const key = e.target
-  const action = key.dataset.action
-  
-  if (!action) {
-    console.log('key')
+
+    // if operator selected
+    if (action === 'add' || action === 'subtract' || action === 'multiply' ||  action === 'divide') {
+      const firstValue = calculator.dataset.firstValue
+      const operator = calculator.dataset.operator
+      const secondValue = displayedNum
+
+      if (firstValue && operator && previousKeyType !== 'operator' && previousKeyType !== 'calculate') {
+        const calcValue = calculate(firstValue, operator, secondValue)
+        display.textContent = calcValue
+        calculator.dataset.firstValue = calcValue
+        } else {
+        calculator.dataset.firstValue = displayedNum
+      }
+
+      key.classList.add('is-depressed')
+      calculator.dataset.previousKeyType = 'operator'
+      calculator.dataset.operator = action
+    }
+
+    // if decimal selected
+    if (action === 'decimal') {
+      if (!displayedNum.includes('.')) {
+        display.textContent = displayedNum + '.'
+      } else if (previousKeyType === 'operator' || previousKeyType === 'calculate') {
+        display.textContent = '0.'
+      }
+      calculator.dataset.previousKeyType = 'decimal'
+    }
+
+    if (action === 'clear') {
+      display.textContent = 0
+    }
+
+    if (action === 'calculate') {
+      let firstValue = calculator.dataset.firstValue
+      const operator = calculator.dataset.operator
+      const secondValue = displayedNum
+
+      if (firstValue) {
+        if (previousKeyType === 'calculate') {
+          firstValue = displayedNum
+          secondValue = calculator.dataset.modValue
+        }
+        display.textContent = calculate(firstValue, operator, secondValue)
+      }
+      calculator.dataset.modValue = secondValue
+      calculator.dataset.previousKeyType = 'calculate'
+    } 
   }
-  if (action === 'add' || action === 'substract' || action === 'multiply' || action === 'divide') {
-    console.log('type of operator')
-  }
-  if (action === 'decimal') {
-    console.log('decimal')
-  }
-  if (action === 'clear') {
-    console.log('clear')
-  }
-  if (action === 'calculate') {
-    console.log('calculate')
-  } 
-  
+    
 })    
 
  
